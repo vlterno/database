41e213e056286a3db18dbba4a6bd6aebeab5a851
vlterno <vlterno@gmail.com>
2016-12-06 14:47:43 +0300

    fix views.py

--- devicedb/views.py
+++ devicedb/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Pc, Notebook, Smartphone, Product
 
 def devicedb_list(request):
-    return render(request, 'devicedb/devicedb_list.html', {})
+	pcs = Pc.objects.order_by("model_name")
+	notebooks = Notebook.objects.order_by("model_name")
+	smartphones = Smartphone.objects.order_by("model_name")
+	products = Product.objects.order_by("model_name")
+	return render(request, 'devicedb/devicedb_list.html', {
+		'pcs': pcs, 
+		'notebooks': notebooks, 
+		'smartphones': smartphones, 
+		'products': products
+	})
